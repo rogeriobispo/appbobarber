3ef8976cc829973c397c5ce06aa3f96d380fc003
Rogério Bispo <rogerio_pd@yahoo.com.br>
2020-07-16 19:15:21 -0300

    dashboard done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Button } from 'react-native';
+import { useAuth } from '../../hooks/Auth';
 
-const Dashboard: React.FC = () => <View />;
+const Dashboard: React.FC = () => {
+  const { signOUt } = useAuth();
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Button title="Sair" onPress={() => signOUt()}></Button>
+    </View>
+  );
+};
 
 export default Dashboard;
